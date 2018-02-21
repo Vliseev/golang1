9d1d4b575341fbbf3a13ff34847075b1f2c1b540
Vliseev <Vliseev.92@mail.ru>
2018-02-21 22:12:30 +0300

    add hw4

---
+++ test/f_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	ID      string
+	Result  *CheckoutResult
+	IsError bool
+}
+
+
+func TestCheckout(t *testing.T) {
+	cases := []TestCase{
+		TestCase{
+			ID: "42",
+			Result: &CheckoutResult{
+				Status:  200,
+				Balance: 100500,
+				Err:     "",
+			},
+			IsError: false,
+		},
+		TestCase{
+			ID: "100500",
+			Result: &CheckoutResult{
+				Status:  400,
+				Balance: 0,
+				Err:     "bad_balance",
+			},
+			IsError: false,
+		},
+		TestCase{
+			ID:      "__broken_json",
+			Result:  nil,
+			IsError: true,
+		},
+		TestCase{
+			ID:      "__internal_error",
+			Result:  nil,
+			IsError: true,
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(CheckoutDummy))
+
+	for caseNum, item := range cases {
+		c := &Cart{
+			PaymentApiURL: ts.URL,
+		}
+		result, err := c.Checkout(item.ID)
+
+		if err != nil && !item.IsError {
+			t.Errorf("[%d] unexpected error: %#v", caseNum, err)
+		}
+		if err == nil && item.IsError {
+			t.Errorf("[%d] expected error, got nil", caseNum)
+		}
+		if !reflect.DeepEqual(item.Result, result) {
+			t.Errorf("[%d] wrong result, expected %#v, got %#v", caseNum, item.Result, result)
+		}
+	}
+	ts.Close()
+}
